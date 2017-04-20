a5299e29a4a89b11b965df8aba2bed22aeaededd
Vito Bellini <v.bellini@gmail.com>
2017-04-20 16:00:18 +0200

    slave and master configs

--- boot_master.sh
+++ boot_master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-mv /root/conf-hadoop-master $HADOOP_HOME/etc/hadoop/
+cp /root/conf-hadoop-master/* $HADOOP_HOME/etc/hadoop/
 
 mv /root/masters $HADOOP_HOME/etc/hadoop/
 mv /root/slaves $HADOOP_HOME/etc/hadoop/
